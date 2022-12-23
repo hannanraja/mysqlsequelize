f2b3c056dc24356553b736ef4c49060b34a1cc51
Hannan <hannanraja1992@gmail.com>
2022-12-23 16:49:29 +0500

    final

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ app.use(cors());
 app.set('port', port);
 var server = http.createServer(app);
 const sequelize = require('./config.js')
-const User = require('./models/table2');
-const User1 = require('./models/table1');
 const router = require('./services/userServices')
 const productservices = require('./services/productService')
 const models = require('./models/index')
@@ -26,7 +24,6 @@ function onError() {
 function onListening() {
 console.log('Listening')
 }
-
 
 app.get('/', (req, res) => {
     res.status(200).send({
